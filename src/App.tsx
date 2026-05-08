26e0992abb12e7ea28e1aa4e6e6d18b5ba243e65
Safitri Haryanti <safitriharyanti086@gmail.com>
2026-05-08 14:56:54 +0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>KAS Dashboard</h1>
+      <p>Dashboard berhasil berjalan 🚀</p>
+    </div>
+  );
+}
